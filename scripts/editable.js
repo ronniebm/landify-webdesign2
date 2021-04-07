46d400c620392b2e91ed7015903afbf2c0e21d3e
Ronnie <ronnie.coding@gmail.com>
2021-04-07 13:11:22 -0500

    removing two comment lines

--- scripts/editable.js
+++ scripts/editable.js
@@ -5,14 +5,12 @@ let buttonEdit = document.querySelector('.js-edit');
 document.addEventListener ("keydown", function (event) {
   if (event.ctrlKey  &&  event.key === "k") { // === case sensitive.
       buttonEdit.style.display = 'block';
-      // buttonEdit.classList.toggle('edit--warning');
       activateTextEditing(true);
   }
 });
 
 buttonEdit.addEventListener('click', (e)=>{
   buttonEdit.style.display = 'none';
-  // buttonEdit.classList.toggle('edit--warning');
   activateTextEditing(false);
 })
 
